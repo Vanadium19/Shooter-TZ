550453359fb9517a4b7f2b1422180f8af7973c4f
Vladislav <GimliVlad@list.ru>
2026-01-13 02:52:42 +0400

    Update sensor

--- Assets/_Main/Scripts/AIModule/Sensors/PlayerSensor.cs
+++ Assets/_Main/Scripts/AIModule/Sensors/PlayerSensor.cs
@@ -1,3 +1,5 @@
+using System;
+using CoreModule;
 using EntityModule;
 using PlayerModule;
 using UnityEngine;
@@ -7,15 +9,19 @@ namespace AIModule
 {
     public class PlayerSensor : MonoBehaviour
     {
-        private Blackboard _blackboard;
+        [SerializeField] private float viewAngle = 90f;
+        [SerializeField] private float viewDistance = 12f;
+        [SerializeField] private LayerMask obstacleMask;
 
-        [Inject]
-        public void Construct(Blackboard blackboard)
+        private Blackboard _blackboard;
+        private IGameService _gameService;
+
+        private void OnDestroy()
         {
-            _blackboard = blackboard;
+            _gameService.Restarted -= DeleteTarget;
         }
 
-        private void OnTriggerEnter(Collider other)
+        private void OnTriggerStay(Collider other)
         {
             if (!other.TryGetComponent(out IEntity entity))
                 return;
@@ -23,7 +29,55 @@ namespace AIModule
             if (!entity.TryGet(out PlayerTag playerTag))
                 return;
 
-            _blackboard.SetObject(BlackboardTag.Target, entity.Get<Transform>());
+            var target = entity.Get<Transform>();
+
+            if (!IsInSight(target, other))
+                return;
+
+            _blackboard.SetObject(BlackboardTag.Target, target);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (!other.TryGetComponent(out IEntity entity))
+                return;
+
+            if (!entity.TryGet(out PlayerTag playerTag))
+                return;
+
+            DeleteTarget();
+        }
+
+        [Inject]
+        public void Construct(IGameService gameService, Blackboard blackboard)
+        {
+            _blackboard = blackboard;
+            _gameService = gameService;
+            _gameService.Restarted += DeleteTarget;
+        }
+
+        private bool IsInSight(Transform target, Collider targetCollider)
+        {
+            var direction = target.position - transform.position;
+            var distance = direction.magnitude;
+
+            if (distance > viewDistance)
+                return false;
+
+            var angle = Vector3.Angle(transform.forward, direction);
+
+            if (angle > viewAngle * 0.5f)
+                return false;
+
+            if (Physics.Raycast(transform.position, direction.normalized, out var hit, distance, obstacleMask))
+                return hit.collider == targetCollider;
+
+            return true;
+        }
+
+        private void DeleteTarget()
+        {
+            _blackboard.DeleteObject(BlackboardTag.Target);
         }
     }
 }
